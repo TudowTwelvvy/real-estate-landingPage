a24aa3a0e12d7aa1a1e8b9075e21c0a034bcb1cf
Tumelo Khanye <tumelokhanye9811@gmail.com>
2024-07-24 11:53:22 +0200

    completed the properties component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -28,7 +28,7 @@ function Hero() {
             data-aos="zoom-in"
             className="text-6xl text-white font-semibold lg:pr-[500px] pr-0 lg:leading-[70px] leading-[60px]"
           >
-            Find your next Home in Johannesburg
+            Find your next Home Gauteng
           </h1>
           <p
             data-aos="zoom-in"
@@ -53,7 +53,76 @@ function Hero() {
           className={`${
             darkMode ? 'dark bg-gray-800' : 'light bg-white'
           } lg:w-[70%] w-full m-auto grid lg:grid-cols-4 grid-cols-1 justify-center items-center gap-6 p-8 rounded-xl -mt-14`}
-        ></div>
+        >
+          <div className="w-full">
+            <h1 className="text-black font-semibold dark:text-white">
+              LOCATION
+            </h1>
+            <input
+              type="text"
+              placeholder="Enter and address, state,city or postal-code"
+              className="bg-white p-2 w-full mt-2 border-b-[1px] border-[#c9c7c1]"
+            />
+          </div>
+
+          <div className="w-full ">
+            <h1 className="text-black font-semibold dark:text-white">TYPE</h1>
+            <select
+              name="selectOption"
+              id="selectOption"
+              className="bg-white p-2 border-b-[1px] w-full mt-2 border-[#c9c7c1] text-gray-500 text-md"
+            >
+              <option value="" className="w" disabled selected>
+                Select Property
+              </option>
+              <option value="Option1" className="w">
+                Rentals
+              </option>
+              <option value="Option2" className="w">
+                Sales
+              </option>
+              <option value="Option3" className="w">
+                Commercial
+              </option>
+            </select>
+          </div>
+
+          <div className="w-full ">
+            <h1 className="text-black font-semibold dark:text-white">
+              CATEGORY
+            </h1>
+            <select
+              name="selectOption"
+              id="selectOption"
+              className="bg-white p-2 border-b-[1px] w-full mt-2 border-[#c9c7c1] text-gray-500 text-md"
+            >
+              <option value="" className="w" disabled selected>
+                Property Category
+              </option>
+              <option value="Option1" className="w">
+                Apartments
+              </option>
+              <option value="Option2" className="w">
+                Duplets
+              </option>
+              <option value="Option3" className="w">
+                Condos
+              </option>
+              <option value="Option4" className="w">
+                Condos
+              </option>
+              <option value="Option5" className="w">
+                Condos
+              </option>
+            </select>
+          </div>
+
+          <div className="w-full">
+            <button className="bg-red-600 dark:bg-red-700 hover:bg-black dark:hover:bg-white dark:hover:text-black text-lg p-4 w-full text-white font-semibold rounded-xl cursor-pointer transform hover:scale-110 transition-transform duration-300">
+              SUBMIT
+            </button>
+          </div>
+        </div>
       </div>
     </>
   )
--- src/sections/PopularAreas.jsx
+++ src/sections/PopularAreas.jsx
@@ -1,7 +1,108 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDarkMode } from '../components/DarkModeContext'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import areaImg1 from '../assets/images/area1.jpg'
+import areaImg2 from '../assets/images/area2.jpg'
+import areaImg3 from '../assets/images/area3.jpg'
 
 function PopularAreas() {
-  return <div>PopularAreas</div>
+  useEffect(() => {
+    AOS.init({
+      offset: 200,
+      duration: 800,
+      easing: 'ease-in-sine',
+      delay: 100,
+    })
+  }, [])
+
+  const { darkMode, toggleDark } = useDarkMode()
+
+  return (
+    <div className={`${darkMode ? 'dark bg-black' : 'light bg-transparent'}`}>
+      <section
+        className={`${
+          darkMode ? 'dark bg-gray-800' : 'light bg-red-100'
+        } lg:w-[90%] w-full h-fit m-auto bg-cover bg-center rounded-xl flex justify-center flex-col items-center lg:px-20 px-6 py-20 gap-20`}
+      >
+        <div
+          id="top"
+          className="w-full grid lag:grid-cols-3 lg:grid-cols-3 grid-cols-1 justify-center items-center gap-8"
+        >
+          <div>
+            <h1 data-aos="zoom-in" className="text-red-500 dark:text-white">
+              POPULAR AREAS
+            </h1>
+            <h1
+              data-aos="zoom-in"
+              className="text-black text-[40px] font-semibold leading-10 mt-4 dark:text-white"
+            >
+              Explore most <br></br> popular areas
+            </h1>
+          </div>
+
+          <div className="grid lg:grid-cols-3 col-span-2 grid-cols-1 justify-center items-center gap-6">
+            <div
+              data-aos="zoom-in"
+              data-aos-delay="400"
+              className="h-[400px] bg-cover bg-center rounded-xl"
+              style={{ backgroundImage: `url(${areaImg1})` }}
+            ></div>
+            <div
+              data-aos="zoom-in"
+              data-aos-delay="400"
+              className="h-[400px] bg-cover bg-center rounded-xl"
+              style={{ backgroundImage: `url(${areaImg2})` }}
+            ></div>
+            <div
+              data-aos="zoom-in"
+              data-aos-delay="400"
+              className="h-[400px] bg-cover bg-center rounded-xl"
+              style={{ backgroundImage: `url(${areaImg3})` }}
+            ></div>
+          </div>
+        </div>
+        <div className="w-full grid lg:grid-cols-3 grid-cols-1 lg:justify-center justify-start items-start gap-6">
+          <div
+            data-aos="slide-up"
+            data-aos-delay="200"
+            className="flex justify-center lg:items-center gap-8 w-full"
+          >
+            <h1 className="text-black text-5xl font-semibold dark:text-white">
+              5K
+            </h1>
+            <h3 className="w">
+              ACTIVE <br /> LISTINGS
+            </h3>
+          </div>
+          <div
+            data-aos="slide-up"
+            data-aos-delay="200"
+            className="flex justify-center lg:items-center gap-8 w-full"
+          >
+            <h1 className="text-black text-5xl font-semibold dark:text-white">
+              5K
+            </h1>
+            <h3 className="w">
+              ACTIVE <br /> LISTINGS
+            </h3>
+          </div>
+          <div
+            data-aos="slide-up"
+            data-aos-delay="200"
+            className="flex justify-center lg:items-center gap-8 w-full"
+          >
+            <h1 className="text-black text-5xl font-semibold dark:text-white">
+              5K
+            </h1>
+            <h3 className="w">
+              ACTIVE <br /> LISTINGS
+            </h3>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
 }
 
 export default PopularAreas
