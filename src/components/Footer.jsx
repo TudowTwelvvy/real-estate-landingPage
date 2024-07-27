146bca90f081f77590e06dbd3ce6d98b58080764
Tumelo <122713460+TudowTwelvvy@users.noreply.github.com>
2024-07-28 00:07:59 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -84,7 +84,7 @@ function Footer() {
       <div className="w">
         <button
           onClick={toggleDarkMode}
-          className="flex items-center p-4 rounded-full bg-orange-500 fixed lg:top-52 right-6 top-6"
+          className="flex items-center p-4 rounded-full bg-orange-500 fixed lg:top-52 right-6 top-10"
         >
           {darkMode ? (
             <FaMoon size={25} className="text-black" />
